1361e909315a569cd9b9a3e37dc021d1818fc9ec
Jacques Terblanche <jacques.terblanche@idrgroup.com>
2024-06-14 12:31:01 +0200

    remove bungee font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,5 @@
 import { Inter } from 'next/font/google';
-import {Bungee_Spice} from 'next/font/google';
-import {Lusitana}  from 'next/font/google';
+import { Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
-export const bungeeSpice = Bungee_Spice({weight:"400", subsets:['latin']});
-export const lusitana = Lusitana({weight:"400", subsets:['latin']});
+export const lusitana = Lusitana({ weight: "400", subsets: ['latin'] });
